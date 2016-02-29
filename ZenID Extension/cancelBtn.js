b131e09f3b84dd35fbb7b4a1a2fc6a817c0b7f21
Chen Li <yichen.li0830@gmail.com>
2016-02-29 15:38:47 -0330

    Adding gulp to support extension live watch && auto minify and uglify scripts

--- ZenID Extension/cancelBtn.js
+++
@@ -1,6 +0,0 @@
-(function () {
-    var cancelBtn = document.getElementById('scriptCancel');
-    cancelBtn.onclick = function () {
-        chrome.runtime.sendMessage({message:"stop_login"});
-    };
-})();
